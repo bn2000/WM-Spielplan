078e48c2ef6b543afbee0e9937db55eac43bafbd
Bruno Neto <bruno-neto@gmx.net>
2018-06-27 12:03:17 +0200

    Gemeinsames Projekt angelegt

---
+++ Mannschaft.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Mannschaft.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Mannschaft
+{
+    
+
+
+    /**
+     * Konstruktor für Objekte der Klasse Mannschaft
+     */
+    public Mannschaft()
+    {
+        // Instanzvariable initialisieren
+        
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        
+    }
+}
---
+++ Spiel.java
@@ -0,0 +1,44 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Spiel.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Spiel
+{
+    private int datum;
+    private int heimtore;
+    private int gasttore;
+    private String ort;
+    private String heimmannschaft;
+    private String gastmannschaft;
+
+    /**
+     * Konstruktor für Objekte der Klasse Spiel
+     */
+    public Spiel(int )
+    {
+        this.datum=datum;
+        this.heimtore=heimtore;
+        this.gasttore=gasttore;
+        this.ort=ort;
+        this.heimmannschaft=heimmannschaft;
+        this.gastmannschaft=gastmannschaft;
+        
+       
+        
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
